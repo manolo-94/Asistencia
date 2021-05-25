9fb9bd6ba1329ad7f5b0abbd44f4c0f423ad6646
luis.villanueva <manolovp94@gamil.com>
2021-05-24 23:52:35 -0500

    diseño de abrir y cerrar casilla

--- src/app/pages/casilla/casilla.page.ts
+++ src/app/pages/casilla/casilla.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2/dist/sweetalert2.js'
 
 @Component({
   selector: 'app-casilla',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CasillaPage implements OnInit {
 
+  status:boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openmodal()
+  {
+    
+    this.status = true;
+    console.log(this.status)
+    Swal.fire({
+      title: 'Casilla!!',
+      text:   "La casilla se ha abierto.",
+      icon: 'success'
+    }); 
+  }
+
+  closemodal()
+  {
+    this.status = false;
+    console.log(this.status)
+    Swal.fire({
+      title: 'Casilla!!',
+      text:   "La casilla se ha cerrado.",
+      icon: 'error'
+    });  
   }
 
 }
