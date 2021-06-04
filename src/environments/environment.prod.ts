afa4342040aa33ebfe04c2705b67662d8de96305
luis.villanueva <manolovp94@gamil.com>
2021-06-04 15:54:49 -0500

    modificaicones para compilar la app en modo prod release y no debug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  url_respaldo:'https://sipba.mx/api',
+  // url:'http://10.0.2.40:8000/api'
+  // url:'https://f0cc7dd2b6f4.ngrok.io/api'
+  url:'https://sipba.g0o.mx/api'
 };
